4f536f92d953136584a1e30628993ce040b6effb
waltonnya <117782227+waltonnya@users.noreply.github.com>
2022-11-08 17:50:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
-// write your code here
+// Nya Walton   J00894068  11/8/2022
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+  {
+    int a;
+    int b;
+    int c;
+    int discriminant;
+    
+    cout << "Input value of a: ";
+    cin >> a;
+    cout << endl;
+    
+    cout << "Input value of b: ";
+    cin >> b;
+    cout << endl;
+    
+    cout << "Input value of c: ";
+    cin >> c;
+    cout << endl;
+    
+    discriminant = pow(b, 2) - (4*a*c);
+    
+    if (discriminant == 0)
+    {
+        cout << "The equation has a single expected root.";
+    }
+ 
+    else if (discriminant > 0 )
+    { cout << "The equation has two real roots.";
+    }
+       else if (discriminant < 0)
+    {  cout << "The equation has two complex roots.";}
+        
+        
+    
+    return 0;
+}
+  
