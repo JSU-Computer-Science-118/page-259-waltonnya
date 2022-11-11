4ad035818db3282f8b6165271dea49bbb9ae8e18
waltonnya <117782227+waltonnya@users.noreply.github.com>
2022-11-10 18:57:15 -0600

    jj

--- main.cpp
+++ main.cpp
@@ -1,46 +1,44 @@
 // Nya Walton   J00894068  11/8/2022
-#include <iostream>
+
+
+   #include <iostream>
 #include <cmath>
+
 using namespace std;
-int main()
-  {
-    int a;
-    int b;
-    int c;
-    int discriminant;
-    
-    cout << "Input value of a: ";
-    cin >> a;
-    cout << endl;
+
+int main ()
+
+{
+
+
+  double a;
+  double b;
+  double c;
+  double x;
+  double discriminant;
+
+  cout << "enter the value for a" << endl;
+  cin >> a;
+  cout << "enter the value for b" << endl;
+  cin >> b;
+  cout << "enter the value for c" << endl;
+  cin >> c;
+
+    x = ((b)*(b)-((4)*(a)*(c)));
+  discriminant = pow(x, 0.5);
+
+  cout << "The descriminant is " << discriminant << endl;
+  cout << endl;
   
-  cout << "Input: ";
-  cin >> a >> b >> c;
-    
-   // cout << "Input value of b: ";
-   // cin >> b;
-  //  cout << endl;
-    
-  //  cout << "Input value of c: ";
- //   cin >> c;
- //   cout << endl;
-    
-   discriminant = pow(b, 2) - (4*a*c);
-    
-   if (discriminant == 0)
-  {
-      cout << "The equation has a single repeated root.";
-   }
- 
-   else if (discriminant > 0 )
-   { cout << "The equation has two real roots./n";
-    }
-       else if (discriminant < 0)
-    {  cout << "Complex Roots./n";}
-  else if (discriminant >= 0)
-    cout << "The roots are";
-        
-        
-    
-    return 0;
+if (discriminant >= 0)
+{
+  if (discriminant = 0)
+     cout << "There is one real solution" << endl;
+  else 
+     cout << "There are at least two real solutions" << endl;
 }
+else
+   cout << "There are no real solutions" << endl;
   
+  return 0;
+  }
